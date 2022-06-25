01d05795135422868da5414fa7b734eff1d3d13e
Alexandr Ilinskiy <alexandr.rondyrev-ilinskiy@touchin.ru>
2022-06-25 16:34:26 +0300

    delete imports

--- src/main/kotlin/com/example/plugins/Security.kt
+++ src/main/kotlin/com/example/plugins/Security.kt
@@ -1,16 +1,13 @@
 package com.example.plugins
 
-import io.ktor.server.auth.*
-import io.ktor.util.*
 import io.ktor.client.*
 import io.ktor.client.engine.apache.*
-import io.ktor.server.locations.*
 import io.ktor.http.*
-import io.ktor.server.sessions.*
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
 import io.ktor.server.response.*
-import io.ktor.server.request.*
 import io.ktor.server.routing.*
+import io.ktor.server.sessions.*
 
 fun Application.configureSecurity() {
     
