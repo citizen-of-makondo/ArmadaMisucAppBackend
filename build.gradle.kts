35509c5f1792c01b7b8cb963120e3a2b0d55a603
Alexandr Ilinskiy <alexandr.rondyrev-ilinskiy@touchin.ru>
2022-06-30 19:51:20 +0300

    Add database dependencies and setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val exposed_version: String by project
 
 plugins {
     application
@@ -31,6 +32,13 @@ dependencies {
     implementation("io.ktor:ktor-client-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-client-apache-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+
+    // DB
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.postgresql:postgresql:42.2.2")
+
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -3,8 +3,18 @@ package com.example
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.example.plugins.*
+import io.ktor.server.util.*
+import org.jetbrains.exposed.sql.*
 
 fun main() {
+
+    Database.connect(
+        url = "ec2-34-242-84-130.eu-west-1.compute.amazonaws.com:5432/d4s9f41la5s1vs",
+        driver = "org.postgresql.Driver",
+        user = "unsmgdpzihlfve",
+        password = "8815c5dd3cf2574a090c1fda93c67ef11c2a8a14b9c1584c59feb52ecaaf901e"
+    )
+
     embeddedServer(Netty, port = System.getenv("PORT").toInt()) {
         configureRouting()
         configureSerialization()
